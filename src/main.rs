e602906f360f6cfeffa4d6603cf153480016fb3e
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-04-11 21:46:18 +0200

    feat: Add ability to load files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{env, process};
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        eprintln!("ERROR: No files provided");
+        process::exit(1);
+    }
+
+    // TODO: Support multiple files
+    let filename = args[1].clone();
+
+    let file = match File::open(filename) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("ERROR: {}", e);
+            process::exit(1);
+        }
+    };
+
+    dbg!(file);
 }
